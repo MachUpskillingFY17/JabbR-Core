5f50c06fce6e26b17d8abf6b50328523411606c1
Adam Tuliper <adamt@microsoft.com>
2016-08-29 16:39:01 -0700

    Chat hub and settings changes

---
+++ src/JabbR-Core/Hubs/Chat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace JabbR_Core.Hubs
+{
+    public class Chat : Hub
+    {
+        public void Join()
+        {
+            //Simple test to see if server is hit from client
+            Clients.Caller.logOn(new object[0], new object[0], new { TabOrder = new List<string>() });
+        }
+    }
+}
