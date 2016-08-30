1986ec47a4638bb33fa81b7fc5113633ac0a5282
Heather Shapiro <hshapiro@microsoft.com>
2016-08-30 15:57:56 -0700

    Updated chat.cs

--- src/JabbR-Core/Hubs/Chat.cs
+++ src/JabbR-Core/Hubs/Chat.cs
@@ -3,15 +3,118 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
+using JabbR_Core.ViewModels;
 
 namespace JabbR_Core.Hubs
 {
+
     public class Chat : Hub
     {
+        public List<string> rooms;
         public void Join()
         {
             //Simple test to see if server is hit from client
             Clients.Caller.logOn(new object[0], new object[0], new { TabOrder = new List<string>() });
+            //Clients.Caller.GetRooms(rooms);
+            //Clients.Caller.GetCommands(new object[0]);
+            //Clients.Caller.GetShortCuts(new object[0]);
+            //Clients.Caller.LoadRooms(new object[0]);
+            //Clients.Caller.updateActivity(new UserData() {
+            //    name = "heather",
+            //    hash = null,
+            //    owner = null,
+            //    active = null,
+            //    noteClass = null,
+            //    note = null,
+            //    flagClass = null,
+            //    flag = null,
+            //    country = null,
+            //    lastActive = "2016-08-23 00:26:35.713",
+            //    timeAgo = null,
+            //    admin = true,
+            //    afk = true,
+            // }
+            //);
+        }
+
+        public LobbyRoomViewModel GetRooms()
+        {
+            //List<string> rooms;
+            UserData user = new UserData()
+            {
+                name = "heather",
+                hash = null,
+                owner = null,
+                active = null,
+                noteClass = null,
+                note = null,
+                flagClass = null,
+                flag = null,
+                country = null,
+                lastActive = "2016-08-23 00:26:35.713",
+                timeAgo = null,
+                admin = true,
+                afk = true,
+            };
+            return
+                new LobbyRoomViewModel
+                {
+                    Name = user.name,
+                    Count = 1,
+                    Private = false,
+                    Closed = false,
+                    Topic = null
+                };
+
+        }
+        public void GetCommands()
+        {
+
+        }
+        public object GetShortCuts()
+        {
+            return new[] {
+                new { Name = "Tab or Shift + Tab", Group = "shortcut", IsKeyCombination = true, Description = LanguageResources.Client_ShortcutTabs },
+                new { Name = "Alt + L", Group = "shortcut", IsKeyCombination = true, Description = LanguageResources.Client_ShortcutLobby },
+                new { Name = "Alt + Number", Group = "shortcut", IsKeyCombination = true, Description = LanguageResources.Client_ShortcutSpecificTab }
+            };
+        }
+        public void LoadRooms()
+        {
+
+        }
+        public void updateActivity()
+        {
+            //string userId = "1";
+
+            UserData user = new UserData()
+                {
+                    name = "heather",
+                    hash = null,
+                    owner = null,
+                    active = null,
+                    noteClass = null,
+                    note = null,
+                    flagClass = null,
+                    flag = null,
+                    country = null,
+                    lastActive = "2016-08-23 00:26:35.713",
+                    timeAgo = null,
+                    admin = true,
+                    afk = true,
+                };
+
+            //foreach (var room in user.Rooms)
+            //{
+                UpdateActivity(user, rooms);
+            //}
+
+            //CheckStatus();
+        }
+
+        private void UpdateActivity(UserData user, List<string> rooms)
+        {
+            
         }
     }
 }
---
+++ src/JabbR-Core/userData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JabbR_Core
+{
+    public class UserData
+    {
+        // members
+        public string name;
+        public string hash;
+        public string owner;
+        public string active;
+        public string noteClass;
+        public string note;
+        public string flagClass;
+        public string flag;
+        public string country;
+        public string lastActive;
+        public string timeAgo;
+        public bool admin;
+        public bool afk;
+                                                                  
+    }
+}
