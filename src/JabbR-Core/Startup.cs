e70af9854b54df26604333674c8ba8609a547a81
JamesEarle <j_earle@hotmail.com>
2016-08-30 11:09:41 -0700

    reference to appsettings.json in Startup constructor. SecretsManager CLI enabled.

--- src/JabbR-Core/Startup.cs
+++ src/JabbR-Core/Startup.cs
@@ -8,6 +8,25 @@ namespace JabbR_Core
 {
     public class Startup
     {
+        public IConfigurationRoot Configuration { get; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+            if(env.IsDevelopment())
+            {
+                builder.AddUserSecrets();
+            }
+
+            builder.AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
