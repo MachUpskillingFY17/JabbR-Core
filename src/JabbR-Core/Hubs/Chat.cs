8144a79a86beb4dc843b7e80aa4d148d9d4f0d6a
Kevin Leung <keleung@microsoft.com>
2016-08-31 17:01:36 -0700

    successfully Invoke chat.LoadRooms

--- src/JabbR-Core/Hubs/Chat.cs
+++ src/JabbR-Core/Hubs/Chat.cs
@@ -1,4 +1,5 @@
-﻿using JabbR_Core.Models;
+﻿using System;
+using JabbR_Core.Models;
 using JabbR_Core.ViewModels;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.SignalR;
@@ -8,16 +9,49 @@ namespace JabbR_Core.Hubs
 
     public class Chat : Hub
     {
+        // Mock model instances to pass into Hub methods
         public List<string> ChatRooms;
-        //List<string> rooms;
         public ChatUser User;
+        public string RoomNames;
+        public UserViewModel UserModel;
 
+        // Mock List for LoadRooms()
+        public ChatRoom Room;
+        public List<ChatRoom> RoomList;
+    
+        // Mock List for GetRoom()
+        public List<LobbyRoomViewModel> LobbyRoomList;
+        public LobbyRoomViewModel LobbyRoomView;
+
+        // Constructor populates mock data
         public Chat()
         {
-            User = new ChatUser();
-            User.Name = "light_meow";
-            User.Admin = true;
-            User.Afk = true;
+            // populate ChatUser
+            User = new ChatUser
+            {
+                Name = "user1",
+                LastActivity = Convert.ToDateTime("2016-08-23 00:26:35.713"),
+                IsAdmin = true,
+                IsAfk = true
+            };
+
+            // instantiate UserViewModel object from User
+            UserModel = new UserViewModel(User);
+
+            // populate ChatRoom and RoomList
+            Room = new ChatRoom {Name = "light_meow"};
+            RoomList = new List<ChatRoom> {Room};
+            
+
+            // populate RoomView
+            LobbyRoomView = new LobbyRoomViewModel
+            {
+                Name = Room.Name,
+                Count = 1,
+            };
+
+            // Add RoomView to RoomList
+            LobbyRoomList = new List<LobbyRoomViewModel> {LobbyRoomView};
         }
 
         public void Join()
@@ -28,15 +62,7 @@ namespace JabbR_Core.Hubs
 
         public List<LobbyRoomViewModel> GetRooms()
         {
-            var rooms = new List<LobbyRoomViewModel>();
-            
-            rooms.Add(new LobbyRoomViewModel
-            {
-                Name = User.Name,
-                Count = 1,
-            });
-
-            return rooms;
+            return LobbyRoomList;
         }
 
         public void GetCommands()
@@ -53,9 +79,38 @@ namespace JabbR_Core.Hubs
             };
         }
 
-        public void LoadRooms()
+        public void LoadRooms(string[] roomNames)
         {
+            // Can't async whenall because we'd be hitting a single 
+            // EF context with multiple concurrent queries.
+            foreach (var room in RoomList)
+            {
+                if (room == null || (room.Private && !User.AllowedRooms.Contains(room)))
+                {
+                    continue;
+                }
 
+
+                var roomInfo = new RoomViewModel
+                {
+                    Name = "light_meow"
+                };
+
+                //while (true)
+                //{
+                //    try
+                //    {
+                //        // If invoking roomLoaded fails don't get the roomInfo again
+                //        // roomInfo = roomInfo ?? await GetRoomInfoCore(room);
+                //        Clients.Caller.roomLoaded(roomInfo);
+                //        break;
+                //    }
+                //    catch (Exception ex)
+                //    {
+                //        // logger.Log(ex);
+                //    }
+                //}
+            }
         }
 
         public void UpdateActivity()
--- src/JabbR-Core/Models/ChatUser.cs
+++ src/JabbR-Core/Models/ChatUser.cs
@@ -1,26 +1,90 @@
-﻿namespace JabbR_Core.Models
-{
-    // Class object to replace repo and EF models.. hard coded in Chat.cs for invoke functions
-    // user data is gathered upon login/auth which populates models
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
+namespace JabbR_Core.Models
+{
     public class ChatUser
     {
-        // members
-        public string Name;
-        public string Hash;
-        public string Owner;
-        public string Active;
-        public string NoteClass;
-        public string Note;
-        public string FlagClass;
-        public string Flag;
-        public string Country;
-        public string LastActive;
-        // used to keep names conventions the same in chat.js line 205
-        public string LastActivity = "2016-08-23 00:26:35.713";
-        public string TimeAgo;
-        public bool Admin;
-        public bool Afk;
-                                                                  
+        [Key]
+        public int Key { get; set; }
+
+        [MaxLength(200)]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        // MD5 email hash for gravatar
+        public string Hash { get; set; }
+        public string Salt { get; set; }
+        public string HashedPassword { get; set; }
+        public DateTime LastActivity { get; set; }
+        public DateTime? LastNudged { get; set; }
+        public int Status { get; set; }
+
+        [StringLength(200)]
+        public string Note { get; set; }
+
+        [StringLength(200)]
+        public string AfkNote { get; set; }
+
+        public bool IsAfk { get; set; }
+
+        [StringLength(255)]
+        public string Flag { get; set; }
+
+        // TODO: Migrate everyone off identity and email
+        public string Identity { get; set; }
+
+        public string Email { get; set; }
+
+        public bool IsAdmin { get; set; }
+        public bool IsBanned { get; set; }
+
+        // Request password reset token
+        public string RequestPasswordResetId { get; set; }
+        public DateTimeOffset? RequestPasswordResetValidThrough { get; set; }
+
+        public string RawPreferences { get; set; }
+
+        // List of clients that are currently connected for this user
+        //public virtual ICollection<ChatUserIdentity> Identities { get; set; }
+        //public virtual ICollection<ChatClient> ConnectedClients { get; set; }
+        public virtual ICollection<ChatRoom> OwnedRooms { get; set; }
+        public virtual ICollection<ChatRoom> Rooms { get; set; }
+
+        //public virtual ICollection<Attachment> Attachments { get; set; }
+        //public virtual ICollection<Notification> Notifications { get; set; }
+
+        // Private rooms this user is allowed to go into
+        public virtual ICollection<ChatRoom> AllowedRooms { get; set; }
+
+        //public ChatUser()
+        //{
+        //    Identities = new SafeCollection<ChatUserIdentity>();
+        //    ConnectedClients = new SafeCollection<ChatClient>();
+        //    OwnedRooms = new SafeCollection<ChatRoom>();
+        //    Rooms = new SafeCollection<ChatRoom>();
+        //    AllowedRooms = new SafeCollection<ChatRoom>();
+        //    Attachments = new SafeCollection<Attachment>();
+        //    Notifications = new SafeCollection<Notification>();
+        //}
+
+        //public bool HasUserNameAndPasswordCredentials()
+        //{
+        //    return !String.IsNullOrEmpty(HashedPassword) && !String.IsNullOrEmpty(Name);
+        //}
+
+        //[NotMapped]
+        //public ChatUserPreferences Preferences
+        //{
+        //    get
+        //    {
+        //        return ChatUserPreferences.GetPreferences(this);
+        //    }
+
+        //    set
+        //    {
+        //        value.Serialize(this);
+        //    }
+        //}
     }
 }
