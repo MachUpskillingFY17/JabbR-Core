26c2c0b98f54119618ed9287f30cef0b0022a373
JamesEarle <j_earle@hotmail.com>
2016-09-06 09:55:48 -0700

    added temporary logic fix for CL resources. Placeholder class for next phase as well

---
+++ src/JabbR-Core/Localization/ClientResourceManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JabbR_Core.Localization
+{
+    public class ClientResourceManager
+    {
+        // Placeholder class. Will add logic for providing client resources to settings soon.
+    }
+}
