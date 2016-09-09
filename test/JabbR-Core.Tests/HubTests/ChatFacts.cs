1c0da2ef6406c664ad15562ad9eba855a44884cf
Justine Cocchi <jucocchi@microsoft.com>
2016-09-09 13:55:31 -0700

    added structure for future controller and hub unit tests

---
+++ test/JabbR-Core.Tests/HubTests/ChatFacts.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace JabbR_Core.Tests.Hubs
+{
+    public class ChatFacts
+    {
+
+        [Fact]
+        public void JoinRoomTest()
+        {
+            Assert.True(true);
+        }
+
+    }
+}
