971481fc44c2aad46ee959879bf0e1400b27fbc1
gcrev93 <gcrev93@hotmail.com>
2016-09-22 23:31:50 -0500

    Revert "changes to Account controller"
    This reverts commit 29bd29bdd6c15fabafdf4ab0182f57cf23b986a2.

---
+++ src/JabbR-Core/ViewModels/SocialLoginViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using JabbR_Core.Models;
+//using SimpleAuthentication.Core;
+
+namespace JabbR_Core.ViewModels
+{
+    public class SocialLoginViewModel
+    {
+        public SocialLoginViewModel()//IEnumerable<IAuthenticationProvider> configuredProviders, IEnumerable<ChatUserIdentity> userIdentities)
+        {
+         //   ConfiguredProviders = configuredProviders != null ? configuredProviders.Select(x => x.Name) : Enumerable.Empty<string>();
+           // _userIdentities = userIdentities;
+           
+        }
+
+       // private readonly IEnumerable<ChatUserIdentity> _userIdentities;
+        public IEnumerable<string> ConfiguredProviders { get; private set; }
+
+        public bool IsAlreadyLinked(string providerName)
+        {
+            /*  if (_userIdentities == null || !_userIdentities.Any())
+              {
+                  return false;
+              }
+
+              return _userIdentities.Any(x => x.ProviderName.Equals(providerName, StringComparison.InvariantCultureIgnoreCase));*/
+
+            return true;
+        }
+    }
+}
