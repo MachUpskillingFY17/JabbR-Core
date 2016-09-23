bfa45e9da883a7c7cd24f4c66383e0446165ad06
gcrev93 <gcrev93@hotmail.com>
2016-09-22 20:37:42 -0500

    changes to startup.cs

--- src/JabbR-Core/Startup.cs
+++ src/JabbR-Core/Startup.cs
@@ -1,27 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Security.Claims;
 using JabbR_Core.Localization;
 using JabbR_Core.Configuration;
-using JabbR_Core.Infrastructure;
-using JabbR_Core.Middleware;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.EntityFrameworkCore;
-using JabbR_Core.Services;
 using JabbR_Core.Models;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.DataProtection;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http.Authentication;
+using Microsoft.EntityFrameworkCore;
 
 namespace JabbR_Core
 {
     public class Startup
-    {           
+    {
         private IConfigurationRoot _configuration;
 
         public Startup(IHostingEnvironment env)
@@ -31,13 +22,13 @@ namespace JabbR_Core
                 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
                 .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
 
-            if(env.IsDevelopment())
+            if (env.IsDevelopment())
             {
                 builder.AddUserSecrets();
             }
 
             builder.AddEnvironmentVariables();
-            
+
             _configuration = builder.Build();
         }
 
@@ -53,47 +44,26 @@ namespace JabbR_Core
             // 
             // Reference the Configuration API with the key you defined, and your env variable will be referenced.
             string connection = _configuration["connectionString"];
-            //services.AddDbContext<JabbrContext>(options => options.UseSqlServer(connection));
+            services.AddDbContext<JabbrContext>(options => options.UseSqlServer(connection));
 
             services.AddMvc();
             services.AddSignalR();
-            services.AddTransient<IJabbrRepository, InMemoryRepository>();
-            services.AddTransient<IChatService, ChatService>();
-
-
-            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
             // Establish default settings from appsettings.json
-            services.Configure<Configuration.ApplicationSettings>(_configuration.GetSection("ApplicationSettings"));
+            services.Configure<ApplicationSettings>(_configuration.GetSection("ApplicationSettings"));
 
             // Programmatically add other options that cannot be taken from static strings
-            services.Configure<Configuration.ApplicationSettings>(settings => 
+            services.Configure<ApplicationSettings>(settings =>
             {
                 settings.Version = Version.Parse("0.1");
                 settings.Time = DateTimeOffset.UtcNow.ToString();
                 settings.ClientLanguageResources = new ClientResourceManager().BuildClientResources();
             });
         }
-       
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IHttpContextAccessor httpContextAccessor)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-
-            if (env.IsDevelopment())
-            {
-                
-                app.UseCookieAuthentication(new CookieAuthenticationOptions()
-                {
-                    AuthenticationScheme =  Constants.JabbRAuthType,
-                    LoginPath = new PathString("/Account/Unauthorized/"),
-                    AccessDeniedPath = new PathString("/Account/Forbidden/"),
-                    AutomaticAuthenticate = true,
-                    AutomaticChallenge = true,
-                    CookieName = "jabbr.id"
-                });
-                app.UseFakeLogin();
-            }
-
             loggerFactory.AddConsole();
 
             if (env.IsDevelopment())
@@ -101,13 +71,9 @@ namespace JabbR_Core
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseMvcWithDefaultRoute();
+            app.UseMvcWithDefaultRoute(); //Needed for routing of additional controllers; without it, only the home controller will work.
             app.UseStaticFiles();
             app.UseSignalR();
-
-           
         }
     }
-
-
 }
