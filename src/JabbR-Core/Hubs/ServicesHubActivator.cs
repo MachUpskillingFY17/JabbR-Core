ab03f8925a0a6667170dd3ca928dd12854805f4f
JamesEarle <j_earle@hotmail.com>
2016-10-19 11:28:48 -0700

    trying out new instance creation techniques to reoslve from DI container

---
+++ src/JabbR-Core/Hubs/ServicesHubActivator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.AspNetCore.SignalR.Hubs;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace JabbR_Core.Hubs
+{
+    public class ServicesHubActivator : IHubActivator
+    {
+        private readonly IServiceProvider _serviceProvider;
+        private Chat _chat;
+
+        public ServicesHubActivator(IServiceProvider serviceProvider, Chat chat)
+        {
+            _serviceProvider = serviceProvider;
+            _chat = chat;
+        }
+
+        public IHub Create(HubDescriptor descriptor)
+        {
+            if (descriptor == null)
+            {
+                throw new ArgumentNullException("descriptor");
+            }
+
+            if (descriptor.HubType == null)
+            {
+                return null;
+            }
+
+            // This resolves from the DI container
+            return _chat as IHub;
+
+            //return ActivatorUtilities.CreateInstance<Chat>(_serviceProvider);
+
+            // This doesn't
+            //return ActivatorUtilities.CreateInstance(_serviceProvider, descriptor.HubType) as IHub;
+        }
+    }
+}
