b5567bdb5e413b8363be02758c18b782bfaec0e2
Adam Tuliper <adamt@microsoft.com>
2016-10-25 01:00:06 -0700

    Added command line config options for dotnet driver

--- src/JabbR-Core/Program.cs
+++ src/JabbR-Core/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
 
 namespace JabbR_Core
 {
@@ -11,11 +13,17 @@ namespace JabbR_Core
     {
         public static void Main(string[] args)
         {
+
+            var config = new ConfigurationBuilder().AddCommandLine(args).Build();
+
+
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
+                .UseUrls("http://*:5000")
+                .UseConfiguration(config)
                 .Build();
 
             host.Run();
