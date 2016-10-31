6a7c5e5a5fcb7fe7024c065a97e91d48c3b6f99c
Adina Shanholtz <adinas@microsoft.com>
2016-10-31 16:00:26 -0400

    Added Afk Command

---
+++ src/JabbR-Core/Commands/AfkCommand.cs
@@ -0,0 +1,27 @@
+﻿using JabbR_Core.Services;
+using JabbR_Core.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JabbR_Core.Commands
+{
+    [Command("afk", "AFK_CommandInfo", "[note]", "user")]
+    public class AfkCommand : UserCommand
+    {
+        public override void Execute(CommandContext context, CallerContext callerContext, ChatUser callingUser, string[] args)
+        {
+            string message = String.Join(" ", args).Trim();
+
+            ChatService.ValidateNote(message);
+
+            callingUser.AfkNote = String.IsNullOrWhiteSpace(message) ? null : message;
+            callingUser.IsAfk = true;
+
+            context.NotificationService.ChangeAfk(callingUser);
+
+            context.Repository.CommitChanges();
+        }
+    }
+}
