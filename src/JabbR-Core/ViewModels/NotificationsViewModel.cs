a0d7fb65459121749323b262a56bd1fa2c8aaa24
gcrev93 <gcrev93@gmail.com>
2016-11-03 13:23:18 -0500

    inital commit -- need to finish UI

---
+++ src/JabbR-Core/ViewModels/NotificationsViewModel.cs
@@ -0,0 +1,36 @@
+﻿//using Sakura.AspNet;
+using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace JabbR_Core.ViewModels
+{
+    public class NotificationsViewModel
+    {
+        public bool ShowAll { get; set; }
+        public int UnreadCount { get; set; }
+        public int TotalCount { get; set; }
+        public IList<NotificationViewModel> Notifications { get; set; } //IPagedList
+    }
+
+    public class NotificationViewModel
+    {
+        public int NotificationKey { get; set; }
+        public string Message { get; set; }
+        public string FromUserName { get; set; }
+        public string FromUserImage { get; set; }
+        public string RoomName { get; set; }
+        public bool Read { get; set; }
+        public bool HtmlEncoded { get; set; }
+        public DateTimeOffset When { get; set; }
+
+        /// <summary>
+        /// Returns a JSON-approved, ISO-8601 string representation of the time when the notification was received.
+        /// This specific format string comes from Json.Net source
+        /// </summary>
+        public string WhenString
+        {
+            get { return When.ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss.FFFFFFFK", CultureInfo.InvariantCulture); }
+        }
+    }
+}
--- src/JabbR-Core/wwwroot/notifications.js
+++ src/JabbR-Core/wwwroot/notifications.js
@@ -1,6 +1,17 @@
 ﻿(function ($, utility) {
     "use strict";
     
+    var currentSection = document.location.hash || "#unread";
+    $('a[href="' + currentSection + '"]').tab('show');
+
+    // always set the hash when we change tabs
+    $('a[data-toggle="tab"]').on('shown', function (e) {
+        document.location.hash = e.target.hash;
+    });
+
+    // make sure errors look pretty and bootstrap-y
+    $('input.error').closest('.control-group').addClass('error');
+
     var notificationsMode = $('#notifications-container').data('mode'),
         templates = {
             multiline: $('#multiline-content-template')
