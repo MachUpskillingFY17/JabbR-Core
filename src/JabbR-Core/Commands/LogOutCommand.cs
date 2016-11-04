645cd21f5f053149899d722a21925e4eac210d37
Justine Cocchi <jucocchi@microsoft.com>
2016-11-03 23:34:20 -0700

    cleanup comments in logout command

--- src/JabbR-Core/Commands/LogOutCommand.cs
+++ src/JabbR-Core/Commands/LogOutCommand.cs
@@ -7,11 +7,6 @@ namespace JabbR_Core.Commands
     {
         public override void Execute(CommandContext context, CallerContext callerContext, ChatUser callingUser, string[] args)
         {
-            //await _signInManager.SignOutAsync();
-
-            //// redirect to AccountLogin since you are no longer authenticated
-            //return this.Redirect("~/account/login");
-
             context.NotificationService.LogOut(callingUser, callerContext.ClientId);
         }
     }
