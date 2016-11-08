194cc83cb582301c7ab8c886a384647a2389f1cc
Kevin Leung <keleung@microsoft.com>
2016-11-08 11:58:20 -0800

    Brought over AdminCommand.cs

---
+++ src/JabbR-Core/Commands/AdminCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using JabbR_Core.Data.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace JabbR_Core.Commands
+{
+    public abstract class AdminCommand : UserCommand
+    {
+        public override void Execute(CommandContext context, CallerContext callerContext, ChatUser callingUser, string[] args)
+        {
+            if (!callingUser.IsAdmin)
+            {
+                throw new HubException(LanguageResources.AdminRequired);
+            }
+
+            ExecuteAdminOperation(context, callerContext, callingUser, args);
+        }
+
+        public abstract void ExecuteAdminOperation(CommandContext context, CallerContext callerContext, ChatUser callingUser, string[] args);
+    }
+}
