6f37cca3f386f92c1781880d1f1f1af0047402c5
gcrev93 <gcrev93@gmail.com>
2016-11-08 18:54:43 -0600

    changes to MarkAsRead

--- src/JabbR-Core/Controllers/NotificationController.cs
+++ src/JabbR-Core/Controllers/NotificationController.cs
@@ -54,7 +54,7 @@ namespace JabbR_Core.Controllers
                 return this.Redirect("~/Account/Login/");
             }
 
-            var request = new NotificationRequestModel(); 
+            var request = new NotificationRequestModel();
 
             var id = _context.HttpContext.User.GetUserId();
             ChatUser user = _repository.GetUserById(id);
@@ -74,107 +74,105 @@ namespace JabbR_Core.Controllers
         }
 
 
-        /*    [HttpPost]
-            public IActionResult MarkAsRead()//(int num)
-            {
+        [HttpPost]
+        public IActionResult MarkAsRead()//(int num)
+        {
 
-                /*  if (!IsAuthenticated)
+            if (!User.Identity.IsAuthenticated)
+            {
+                // return Forbidden view
+                Response.StatusCode = 403; // HttpStatusCode.Forbidden
+                //return this.Redirect("~/Account/Forbidden/");
+                return this.Redirect("~/Account/Login/");
+            }
+
+          /*  int notificationId = Request.Form.notificationId;
+
+            Notification notification = repository.GetNotificationById(notificationId);
+
+            if (notification == null)
+            {
+                return HttpStatusCode.NotFound;
+            }
+
+            ChatUser user = repository.GetUserById(Principal.GetUserId());
+
+            if (notification.UserKey != user.Key)
+            {
+                return HttpStatusCode.Forbidden;
+            }
+
+            notification.Read = true;
+            //      _repository.CommitChanges();
+
+            UpdateUnreadCountInChat(_repository, notificationService, user);
+
+            //    var response = Response.AsJson(new { success = true });
+
+            // return response;*/
+            return View("index");
+        }
+
+        /*  [HttpPost]
+          public IActionResult MarkAllAsRead()
+          {
+                  if (!IsAuthenticated)
                   {
                       return HttpStatusCode.Forbidden;
                   }
 
-                  int notificationId = Request.Form.notificationId;
+                  ChatUser user = repository.GetUserById(Principal.GetUserId());
+                  IList<Notification> unReadNotifications = repository.GetNotificationsByUser(user).Unread().ToList();
 
-                  Notification notification = repository.GetNotificationById(notificationId);
-
-                  if (notification == null)
+                  if (!unReadNotifications.Any())
                   {
                       return HttpStatusCode.NotFound;
                   }
 
-                  ChatUser user = repository.GetUserById(Principal.GetUserId());
-
-                  if (notification.UserKey != user.Key)
+                  foreach (var notification in unReadNotifications)
                   {
-                      return HttpStatusCode.Forbidden;
+                      notification.Read = true;
                   }
-                ChatUser user = _repository.GetUserById("1");
 
-                int notificationId = 1;//id;
+                  repository.CommitChanges();
 
-                Notification notification = _repository.GetNotificationById(notificationId);
+                  UpdateUnreadCountInChat(repository, notificationService, user);
 
-                notification.Read = true;
-                //      _repository.CommitChanges();
+                  var response = Response.AsJson(new { success = true });
 
-                UpdateUnreadCountInChat(_repository, notificationService, user);
+                  return response;
+              };
 
-                //    var response = Response.AsJson(new { success = true });
+              ChatUser user = _repository.GetUserById("1");
+              IList<Notification> unReadNotifications = _repository.GetNotificationsByUser(user).Unread().ToList();
 
-                // return response;
-                return View("index");
-            }
+              foreach (var notification in unReadNotifications)
+              {
+                  notification.Read = true;
+              }
 
-            [HttpPost]
-            public IActionResult MarkAllAsRead()
-            {/*
-                    if (!IsAuthenticated)
-                    {
-                        return HttpStatusCode.Forbidden;
-                    }
+              repository.CommitChanges();
 
-                    ChatUser user = repository.GetUserById(Principal.GetUserId());
-                    IList<Notification> unReadNotifications = repository.GetNotificationsByUser(user).Unread().ToList();
+              UpdateUnreadCountInChat(_repository, notificationService, user);
 
-                    if (!unReadNotifications.Any())
-                    {
-                        return HttpStatusCode.NotFound;
-                    }
+              //added this
+              var viewModel = new NotificationsViewModel
+              {
+                  ShowAll = false,//request.All
+                  UnreadCount = 0,
+                  //  TotalCount = 2, //was not here
+                  Notifications = null //notifications
+              };
 
-                    foreach (var notification in unReadNotifications)
-                    {
-                        notification.Read = true;
-                    }
+              return View("index", viewModel);
+          }
 
-                    repository.CommitChanges();
-
-                    UpdateUnreadCountInChat(repository, notificationService, user);
-
-                    var response = Response.AsJson(new { success = true });
-
-                    return response;
-                };
-
-                ChatUser user = _repository.GetUserById("1");
-                IList<Notification> unReadNotifications = _repository.GetNotificationsByUser(user).Unread().ToList();
-
-                foreach (var notification in unReadNotifications)
-                {
-                    notification.Read = true;
-                }
-
-                repository.CommitChanges();
-
-                UpdateUnreadCountInChat(_repository, notificationService, user);
-
-                //added this
-                var viewModel = new NotificationsViewModel
-                {
-                    ShowAll = false,//request.All
-                    UnreadCount = 0,
-                    //  TotalCount = 2, //was not here
-                    Notifications = null //notifications
-                };
-
-                return View("index", viewModel);
-            }
-
-            private static void UpdateUnreadCountInChat(InMemoryRepository repository, IChatNotificationService notificationService,
-                                                        ChatUser user)
-            {
-                var unread = repository.GetUnreadNotificationsCount(user);
-                notificationService.UpdateUnreadMentions(user, unread);
-            }*/
+          private static void UpdateUnreadCountInChat(InMemoryRepository repository, IChatNotificationService notificationService,
+                                                      ChatUser user)
+          {
+              var unread = repository.GetUnreadNotificationsCount(user);
+              notificationService.UpdateUnreadMentions(user, unread);
+          }*/
 
         private static List<NotificationViewModel> GetNotifications(IJabbrRepository repository, ChatUser user, bool all = false,
                                                                           int page = 1, int take = 20, string roomName = "light_meow")
@@ -198,52 +196,52 @@ namespace JabbR_Core.Controllers
 
 
 
-             return notificationsQuery.OrderByDescending(n => n.MessageKeyNavigation.When)
-                                      .Select(n => new NotificationViewModel()
-                                      {
-                                          NotificationKey = n.Key,
-                                          FromUserName = n.MessageKeyNavigation.UserKeyNavigation.Name,
-                                          FromUserImage = "Image", //n.Message.User.Hash,
+            return notificationsQuery.OrderByDescending(n => n.MessageKeyNavigation.When)
+                                     .Select(n => new NotificationViewModel()
+                                     {
+                                         NotificationKey = n.Key,
+                                         FromUserName = n.MessageKeyNavigation.UserKeyNavigation.Name,
+                                         FromUserImage = "Image", //n.Message.User.Hash,
                                           Message = n.MessageKeyNavigation.Content,
-                                          HtmlEncoded = n.MessageKeyNavigation.HtmlEncoded,
-                                          RoomName = n.RoomKeyNavigation.Name,
-                                          Read = n.Read,
-                                          When = n.MessageKeyNavigation.When
-                                      })
-                                      .ToList();
+                                         HtmlEncoded = n.MessageKeyNavigation.HtmlEncoded,
+                                         RoomName = n.RoomKeyNavigation.Name,
+                                         Read = n.Read,
+                                         When = n.MessageKeyNavigation.When
+                                     })
+                                     .ToList();
 
             //To test comment out the first return statement and uncomment the one below, so you have dummy notifications. For testing index
             //You should just be seeing the difference between Unread Notifications and All Notifications Tab. No functionality in this branch
 
-         /*   var vm = new NotificationViewModel()
-            {
-                NotificationKey = 1,
-                FromUserName = "Jack",
-                FromUserImage = "Image",
-                Message = "This is the unread test message",
-                HtmlEncoded = true,
-                RoomName = "light-meow",
-                Read = false,
-                When = DateTimeOffset.Now
-            };
+            /*   var vm = new NotificationViewModel()
+               {
+                   NotificationKey = 1,
+                   FromUserName = "Jack",
+                   FromUserImage = "Image",
+                   Message = "This is the unread test message",
+                   HtmlEncoded = true,
+                   RoomName = "light-meow",
+                   Read = false,
+                   When = DateTimeOffset.Now
+               };
 
-            var vm2 = new NotificationViewModel()
-            {
-                NotificationKey = 2,
-                FromUserName = "Jack",
-                FromUserImage = "Image",
-                Message = "This is the read test message ",
-                HtmlEncoded = true,
-                RoomName = "light-meow",
-                Read = true,
-                When = DateTimeOffset.Now
-            };
-            List<NotificationViewModel> notifications = new List<NotificationViewModel>();
+               var vm2 = new NotificationViewModel()
+               {
+                   NotificationKey = 2,
+                   FromUserName = "Jack",
+                   FromUserImage = "Image",
+                   Message = "This is the read test message ",
+                   HtmlEncoded = true,
+                   RoomName = "light-meow",
+                   Read = true,
+                   When = DateTimeOffset.Now
+               };
+               List<NotificationViewModel> notifications = new List<NotificationViewModel>();
 
-            notifications.Add(vm);
-            notifications.Add(vm2);
+               notifications.Add(vm);
+               notifications.Add(vm2);
 
-            return notifications;*/
+               return notifications;*/
         }
 
         private class NotificationRequestModel
