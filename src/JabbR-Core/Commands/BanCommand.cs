4be33abf7fd36ded3320ba36dd993ef6371876da
Kevin Leung <keleung@microsoft.com>
2016-11-15 11:58:50 -0600

    BanCommand.cs and UnBanCommand.cs

---
+++ src/JabbR-Core/Commands/BanCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using JabbR_Core.Services;
+using JabbR_Core.Data.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace JabbR_Core.Commands
+{
+    [Command("ban", "Ban_CommandInfo", "user [reason]", "admin")]
+    public class BanCommand : AdminCommand
+    {
+        public override void ExecuteAdminOperation(CommandContext context, CallerContext callerContext, ChatUser callingUser, string[] args)
+        {
+            if (args.Length == 0)
+            {
+                throw new HubException(LanguageResources.Ban_UserRequired);
+            }
+
+            string targetUserName = args[0];
+
+            ChatUser targetUser = context.Repository.VerifyUser(targetUserName);
+
+            // try to extract the reason
+            string reason = null;
+            if (args.Length > 1)
+            {
+                reason = String.Join(" ", args.Skip(1)).Trim();
+            }
+
+            context.Service.BanUser(callingUser, targetUser);
+            context.NotificationService.BanUser(targetUser, callingUser, reason);
+            context.Repository.CommitChanges();
+        }
+    }
+}
---
+++ src/JabbR-Core/Commands/UnBanCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using JabbR_Core.Services;
+using JabbR_Core.Data.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace JabbR_Core.Commands
+{
+    [Command("unban", "Unban_CommandInfo", "user", "admin")]
+    public class UnbanCommand : AdminCommand
+    {
+        public override void ExecuteAdminOperation(CommandContext context, CallerContext callerContext, ChatUser callingUser, string[] args)
+        {
+            if (args.Length == 0)
+            {
+                throw new HubException(LanguageResources.Unban_UserRequired);
+            }
+
+            string targetUserName = args[0];
+
+            ChatUser targetUser = context.Repository.VerifyUser(targetUserName);
+
+            context.Service.UnbanUser(callingUser, targetUser);
+            context.NotificationService.UnbanUser(targetUser);
+            context.Repository.CommitChanges();
+        }
+    }
+}
