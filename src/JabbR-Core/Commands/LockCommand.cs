50f20f687dbccbaeb31a5d038c1920172281a589
Justine Cocchi <jucocchi@microsoft.com>
2016-11-17 12:48:10 -0800

    added lock room command

---
+++ src/JabbR-Core/Commands/LockCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using JabbR_Core.Services;
+using JabbR_Core.Data.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace JabbR_Core.Commands
+{
+    [Command("lock", "Lock_CommandInfo", "[room]", "room", "Lock_ConfirmMessage")]
+    public class LockCommand : UserCommand
+    {
+        public override void Execute(CommandContext context, CallerContext callerContext, ChatUser callingUser, string[] args)
+        {
+            string roomName = args.Length > 0 ? args[0] : callerContext.RoomName;
+
+            if (String.IsNullOrEmpty(roomName))
+            {
+                throw new HubException(LanguageResources.Lock_RoomRequired);
+            }
+
+            ChatRoom room = context.Repository.VerifyRoom(roomName, mustBeOpen: false);
+
+            context.Service.LockRoom(callingUser, room);
+
+            context.NotificationService.LockRoom(callingUser, room);
+        }
+    }
+}
